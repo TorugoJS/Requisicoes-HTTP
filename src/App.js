5bb8a5de688fd0b00c876a595ceb005df76c8b41
TorugoJS <victor.devfrontend@gmail.com>
2022-08-25 21:16:32 -0300

    requisicoes-http

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import './App.css';
 
+import { useState, useEffect } from "react";
+
+// API
+const url = "http://localhost:3000/products";
+
 function App() {
+
+  const [products, setProducts] = useState([]);
+
+  // 1 - resgatando dados
+  useEffect(() => {
+    async function fetchData() {
+
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data)
+    }
+
+    fetchData();
+  }, []);
+
+  // console.log(products)
+
   return (
     <div className="App">
-     <h1>Lista de produtos</h1>
+      <h1>Lista de produtos</h1>
+      <ul>
+        {products.map((product)=>(
+          <li key={product.id}> {product.name} - {product.price}</li>
+        ))}
+      </ul>
     </div>
   );
 }
