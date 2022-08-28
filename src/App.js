789dca597f86371b0e1a1909a40b81e6335e8613
TorugoJS <victor.devfrontend@gmail.com>
2022-08-28 17:49:05 -0300

    requisicoes-http

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ const url = "http://localhost:3000/products";
 function App() {
 
   const [products, setProducts] = useState([]);
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
 
   // 1 - resgatando dados
   useEffect(() => {
@@ -23,16 +25,58 @@ function App() {
     fetchData();
   }, []);
 
+  // 2 - Adicionando dados
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const product = {
+      name,
+      price,
+    };
+
+    console.log(product)
+  };
+
   // console.log(products)
 
   return (
+
     <div className="App">
       <h1>Lista de produtos</h1>
+
       <ul>
-        {products.map((product)=>(
+        {products.map((product) => (
           <li key={product.id}> {product.name} - {product.price}</li>
         ))}
       </ul>
+      
+      <div className='add-dados'>
+
+        <form onSubmit={handleSubmit}>
+
+          <label>
+            Nome:
+            <input type="text"
+              value={name}
+              name={name}
+              onChange={(e) => setName(e.target.name)} />
+          </label>
+
+          <label>
+            Preço:
+            <input type="text"
+              value={price}
+              name={price}
+              onChange={(e) => setPrice(e.target.price)} />
+          </label>
+
+          <input type="submit" value="Enviar para o json" />
+
+        </form>
+
+      </div>
+
     </div>
   );
 }
